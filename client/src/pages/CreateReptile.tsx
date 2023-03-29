5b68aaf253f27a0006e7ba9208676341e107e3ab
luke Shumway <a02268065@aggies.usu.edu>
2023-03-29 11:21:27 -0600

    almost finished dashboard, finished creating a reptile, worked on reptile page

--- client/src/pages/CreateReptile.tsx
+++ client/src/pages/CreateReptile.tsx
@@ -1,4 +1,79 @@
+import { useContext, useEffect, useState } from "react";
+import { NavContext } from "../contexts/nav";
 
 export const CreateReptile = () => {
-  return <h1>Welcome to the Reptile Creation Page!</h1>
+  const setPage = useContext(NavContext);
+  const [name, setName] = useState("");
+  const [sex, setSex] = useState("M");
+  const [species, setSpecies] = useState("King Snake");
+  const [isDisplayed, setIsDisplayed] = useState(false);
+  
+  useEffect(() => {
+    if (isDisplayed) {
+      const timeout = setTimeout(() => {
+        setIsDisplayed(false);
+      }, 2000);
+      return () => {
+        clearTimeout(timeout);
+      }
+    }
+    return () => {};
+  }, [isDisplayed]);
+
+  async function createReptile() {
+    const body = {
+      species: species.split(" ")[0].toLowerCase() + "_" + species.split(" ")[1].toLowerCase(),
+      name,
+      sex
+    }
+    if (name != "") {
+      const result = await fetch(`http://localhost:3000/reptiles/create`, { // Need to fix the 
+        method: 'post',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(body)
+      });
+      const res = await result.json()
+      console.log(res);
+      if (!res.message) {
+        setPage("dashboard");
+      } else {
+        setIsDisplayed(true);
+      }
+    } else {
+      setIsDisplayed(true);
+    }
+  }
+
+  return (
+    <div className="CreateReptile">
+      <h1>Create a Reptile</h1>
+      <form className="CreateReptile">
+        <div>
+          Name
+          <input value={name} onChange={e => setName(e.target.value)} type="text"/>
+        </div>
+        <div>
+          Sex
+          <select value={sex} onChange={e => setSex(e.target.value)}>
+            <option>M</option>
+            <option>F</option>
+          </select>
+        </div>
+        <div>
+          Species
+          <select value={species} onChange={e => setSpecies(e.target.value)}>
+            <option>King Snake</option>
+            <option>Ball Python</option>
+            <option>Redtail Boa</option>
+            <option>Corn Snake</option>
+          </select>
+        </div>
+        <button onClick={() => createReptile()}>Create</button>
+      </form>
+      <div className={`toast ${isDisplayed ? 'open' : ''}`}>An error occured. Please try again</div>
+    </div>
+  );
 }
--- client/src/pages/Reptile.tsx
+++ client/src/pages/Reptile.tsx
@@ -1,7 +1,74 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
+import { NavContext } from "../contexts/nav";
 import { ReptileIdContext } from "../contexts/repId"
+
+type Schedule = {
+  id: number;
+  reptileId: number;
+  userId: number;
+  type: string;
+  description: string;
+  monday: boolean;
+  tuesday: boolean;
+  wednesday: boolean;
+  thursday: boolean;
+  friday: boolean;
+  saturday: boolean;
+  sunday: boolean;
+}
 
 export const Reptile = () => {
   const reptileId = useContext(ReptileIdContext);
-  return <h1>Welcome to the page for Reptile #{reptileId}</h1>
+  const setPage = useContext(NavContext);
+  const [schedules, setSchedules] = useState<Schedule[]>([]);
+
+  function getSchedules() {
+    fetch(`http://localhost:3000/schedules/${reptileId}`, {
+      method: "get",
+      credentials: "include",
+    })
+    .then(r => {
+      if (!r.ok) {
+        throw new Error(r.statusText)
+      }
+      return r.json();
+    })
+    .then(json => {setSchedules(json.schedules)})
+  }
+  
+  useEffect(() => {
+    getSchedules();
+  }, [schedules]);
+
+  return (
+    <div className="schedules">
+        <h2>Schedules</h2>
+        Schedules for Reptile
+        {
+          schedules.map((schedule) => (
+            <div key={schedule.id} className="schedule">
+              <div>
+                <p>
+                  Description: {schedule.description} for Reptile #{schedule.reptileId}
+                </p><p>
+                  Monday: {schedule.monday ? 'Yes' : 'No' }
+                </p><p>
+                  Tuesday: {schedule.tuesday ? 'Yes' : 'No' }
+                </p><p>
+                  Wednesday: {schedule.wednesday ? 'Yes' : 'No' }
+                </p><p>
+                  Thursday: {schedule.thursday ? 'Yes' : 'No' }
+                </p><p>
+                  Friday: {schedule.friday ? 'Yes' : 'No' }
+                </p><p>
+                  Saturday: {schedule.saturday ? 'Yes' : 'No' }
+                </p><p>
+                  Sunday: {schedule.sunday ? 'Yes' : 'No' }
+                </p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+  )
 }
